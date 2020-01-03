8cab6d48048340311685bb6fab80811b2d6c99e5
Glen Leao <glenleao@hotmail.com>
2020-01-03 10:15:58 -0300

    crud

---
+++ views-cache/categoria-menu.99b29a28757f64e3eca6d8d1654064ab.rtpl.php
@@ -0,0 +1,4 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?> <a class="nav-item nav-link" href="pecas-comemorativas.html">Peças comemorativas</a>
+      <a class="nav-item nav-link" href="divulgacao.html">Divulgação de eventos</a>
+      <a class="nav-item nav-link" href="projeto-grafico.html">Projeto gráfico</a>
+      <a class="nav-item nav-link" href="contato.html"  aria-disabled="true">Contato</a>
